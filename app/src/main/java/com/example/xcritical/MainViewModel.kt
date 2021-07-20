fd89ebd95e2c3d511dc0813da75503117795e911
Anton <aloe21252@gmail.com>
2021-07-20 10:09:13 +0300

    Implement view model and binding

--- app/src/main/java/com/example/xcritical/MainViewModel.kt
+++ app/src/main/java/com/example/xcritical/MainViewModel.kt
@@ -1,18 +1,62 @@
 package com.example.xcritical
 
+import android.app.Application
 import android.util.Patterns
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.regex.Pattern
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    var email = ""
-    var password = ""
+    private val EMAIL_ADDRESS_PATTERN = Pattern.compile(
+        "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])"
+    )
+    private val _isInvalidLiveDataEmail = MutableLiveData<Boolean>()
+    val isInvalidLiveDataEmail: LiveData<Boolean>
+        get() = _isInvalidLiveDataEmail
 
-    fun incorrectEmail() : Boolean {
-        return ( !Patterns.EMAIL_ADDRESS.matcher(email).matches())
+    private val _isInvalidLiveDataPassword = MutableLiveData<Boolean>()
+    val isInvalidLiveDataPassword: LiveData<Boolean>
+        get() = _isInvalidLiveDataPassword
+
+    private val _isPassedValidation = MutableLiveData<Boolean>()
+    val isPassedValidation: LiveData<Boolean>
+        get() = _isPassedValidation
+
+    private val _isChangingEmailText = MutableLiveData<Boolean>()
+    val isChangingEmailText: LiveData<Boolean>
+        get() = _isChangingEmailText
+
+    private val _isChangingPasswordText = MutableLiveData<Boolean>()
+    val isChangingPasswordText: LiveData<Boolean>
+        get() = _isChangingPasswordText
+
+    fun onEmailTextChanged(s: CharSequence,start: Int,before : Int, count :Int){
+        _isChangingEmailText.value = true
     }
 
-    fun incorrectPassword() : Boolean {
-        return password.isEmpty()
+    fun onPasswordTextChanged(s: CharSequence,start: Int,before : Int, count :Int){
+        _isChangingPasswordText.value = true
+    }
+
+    fun validateLogin(email: String?, password: String?){
+        val mail = validateEmail(email)
+        val pass = validatePassword(password)
+
+        _isPassedValidation.value = !(mail!! || pass!!)
+    }
+
+
+    fun validateEmail(email: String?): Boolean? {
+        _isInvalidLiveDataEmail.value = email.isNullOrEmpty() || !EMAIL_ADDRESS_PATTERN.matcher(email).matches()
+        return _isInvalidLiveDataEmail.value
+    }
+
+
+    private fun validatePassword(password: String?): Boolean? {
+        _isInvalidLiveDataPassword.value = password.isNullOrEmpty()
+        return _isInvalidLiveDataPassword.value
     }
 }
