a466a1b4e2cc8647fff8fdde866f1169a1dab316
Anton <aloe21252@gmail.com>
2021-07-29 16:59:26 +0300

    Заглушка на емейл и пароль

--- app/src/main/java/com/example/xcritical/MainViewModel.kt
+++ app/src/main/java/com/example/xcritical/MainViewModel.kt
@@ -13,23 +13,23 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val EMAIL_ADDRESS_PATTERN = Pattern.compile(
         "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])"
     )
-    private val _isInvalidLiveDataEmail = MutableLiveData<Boolean>()
+    private val _isInvalidLiveDataEmail = MutableLiveData<Boolean>().apply { value = true }
     val isInvalidLiveDataEmail: LiveData<Boolean>
         get() = _isInvalidLiveDataEmail
 
-    private val _isInvalidLiveDataPassword = MutableLiveData<Boolean>()
+    private val _isInvalidLiveDataPassword = MutableLiveData<Boolean>().apply { value = true }
     val isInvalidLiveDataPassword: LiveData<Boolean>
         get() = _isInvalidLiveDataPassword
 
-    private val _isPassedValidation = MutableLiveData<Boolean>()
+    private val _isPassedValidation = MutableLiveData<Boolean>().apply { value = true }
     val isPassedValidation: LiveData<Boolean>
         get() = _isPassedValidation
 
-    private val _isChangingEmailText = MutableLiveData<Boolean>()
+    private val _isChangingEmailText = MutableLiveData<Boolean>().apply { value = true }
     val isChangingEmailText: LiveData<Boolean>
         get() = _isChangingEmailText
 
-    private val _isChangingPasswordText = MutableLiveData<Boolean>()
+    private val _isChangingPasswordText = MutableLiveData<Boolean>().apply { value = true }
     val isChangingPasswordText: LiveData<Boolean>
         get() = _isChangingPasswordText
 
